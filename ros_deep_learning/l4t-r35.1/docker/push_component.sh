6b0df3c268ea3b78a15743e985d6c89f2e37ada2
Saar Moseri <saar@cogniteam.com>
2023-05-17 10:05:47 +0300

    pushing xavier image

--- ros_deep_learning/l4t-r35.1/docker/push_component.sh
+++ ros_deep_learning/l4t-r35.1/docker/push_component.sh
@@ -18,6 +18,6 @@ docker buildx inspect --bootstrap
 #
 
 
-docker buildx build --platform linux/arm64,linux/amd64 -t cognimbus/ros-deep-learning:latest --push .
+docker buildx build --platform linux/arm64,linux/amd64 -t cognimbus/ros-deep-learning:xavier-l4t-r35.1 --push .
 
 
